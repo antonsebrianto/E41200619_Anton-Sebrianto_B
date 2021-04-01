2ddd64d7435d21b62ffa256f3888e268c79cc5fa
anton <antonsebrianto525@gmail.com>
2021-04-01 21:19:39 +0700

    Minggu-4

--- Minggu-4/Tugas_W04/Tugas_2_Kelipatan.java
+++ Minggu-4/Tugas_W04/Tugas_2_Kelipatan.java
@@ -6,14 +6,14 @@ package Tugas_W04;
  */
 public class Tugas_2_Kelipatan {
     public static void main(String[] args) {
-        int awal = 0;
+        int awal = 2;
         int akhir = 100;
         System.out.println("          Do__While           ");
         System.out.println("Bilangan kelipatan 2 (0-100)");
         System.out.println("=============================");
         do {
             System.out.print(awal + " ");
-            awal+=2;
+            awal = awal+=2;
         }while (awal <= akhir);
         
     }
--- Minggu-4/Tugas_W04/Tugas_2_Perkalian.java
+++ Minggu-4/Tugas_W04/Tugas_2_Perkalian.java
@@ -6,15 +6,14 @@ package Tugas_W04;
  */
 public class Tugas_2_Perkalian {
     public static void main(String[] args) {
-        int awal = 1;
+        int awal = 2;
         int akhir = 100;
         System.out.println("          Do__While           ");
-        System.out.println("Bilangan Perkalian 2 (1-100)");
+        System.out.println("Bilangan Perkalian 2 (0-100)");
         System.out.println("=============================");
         do {
             System.out.print(awal + " ");
             awal*=2;
         }while (awal <= akhir);
-        
     }
 }
